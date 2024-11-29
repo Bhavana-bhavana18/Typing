898a21419bf452236daf40c1dcfa64c622e35085
Bhavana-bhavana18 <bhavanabhamare181920@gmail.com>
2024-11-29 15:24:45 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
+import { NgIf, NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'typing';
+  randomText = faker.lorem.sentence();
+  enterText = '';
+
+  onInput(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    const value = input.value;
+    this.enterText = value;
+  }
+  compare(randomLetter: string, enterLetter: string) {
+    if (!enterLetter) {
+      return 'pending';
+    }
+    return randomLetter === enterLetter ? 'correct' : 'incorrect';
+  }
 }
